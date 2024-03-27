2f11f3cecd6fb5a51dfda8ea9b7f4da7a85ded8f
Jérémy Boureux <jeremyboureux@gmail.com>
2024-03-27 12:48:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# My Portfolio
+
+Currently under developpement, this project will become my portfolio.
+
+## Tech stack
+
+In the current state, I use Vite with React to generate a static app that I deploy using Docker.
+
+For the main portfolio, I've not yet decided but I'd like to try Next.JS
---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { TriangleAlert } from 'lucide-react'
+import './App.css'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './components/ui/card'
+
+function App() {
+
+
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <Card className='w-fit h-fit'>
+        <CardHeader>
+          <CardTitle>Bienvenue sur jboureux.fr</CardTitle>
+          <CardDescription>Portfolio de développeur de Jérémy Boureux</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <span className='flex flex-row'>
+            <TriangleAlert className='mr-2'/> Ce site web est en cours de maintenanc.
+          </span>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default App
