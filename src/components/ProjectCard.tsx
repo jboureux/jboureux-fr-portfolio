0da1fe16a66880ddd004cdf21ef2efd6543405d9
Jérémy Boureux <jeremyboureux@gmail.com>
2025-12-16 22:18:23 +0100

    fix: add depshield

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,39 +1,49 @@
 import { Project } from "@/schemas/project";
+import { LinkIcon } from "lucide-react";
 import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "./ui/card";
 
 interface ProjectCardProps {
-    project: Project;
+  project: Project;
 }
 
 const ProjectCard = (props: ProjectCardProps) => {
-    return (
-        <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>{props.project.title}</CardTitle>
-                <img
-                    src={props.project.thumbnail}
-                    className="object-fill w-[350px] h-[197px]"
-                ></img>
-            </CardHeader>
-            <CardContent className="min-h-[300px]">
-                <p>{props.project.description}</p>
-            </CardContent>
-            <CardFooter>
-                <div className="flex flex-row flex-wrap gap-1">
-                    {props.project.tags.map((tag, index) => (
-                        <Badge key={index}>{tag}</Badge>
-                    ))}
-                </div>
-            </CardFooter>
-        </Card>
-    );
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>{props.project.title}</CardTitle>
+        <img
+          src={props.project.thumbnail}
+          className="object-fill w-[350px] h-[197px]"
+        ></img>
+      </CardHeader>
+      <CardContent className="min-h-[300px]">
+        <p>{props.project.description}</p>
+        {props.project.link && (
+          <a href={props.project.link} target="_blank">
+            <Button>
+              <LinkIcon className="mr-2 w-5 h-5" />
+              Voir le projet
+            </Button>
+          </a>
+        )}
+      </CardContent>
+      <CardFooter>
+        <div className="flex flex-row flex-wrap gap-1">
+          {props.project.tags.map((tag, index) => (
+            <Badge key={index}>{tag}</Badge>
+          ))}
+        </div>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default ProjectCard;
--- src/schemas/project.ts
+++ src/schemas/project.ts
@@ -1,6 +1,7 @@
 export interface Project {
-    title: string;
-    description: string;
-    tags: string[];
-    thumbnail: string;
+  title: string;
+  description: string;
+  tags: string[];
+  thumbnail: string;
+  link?: string;
 }
